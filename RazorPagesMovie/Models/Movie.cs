b59d04dd7f6579116aa4dbc26a9505a5241e787f
mkoelbel <mariackoelbel@gmail.com>
2024-06-06 15:42:16 -0400

    Add validation to user inputs

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,11 +6,29 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; } // Required by the database for the primary key.
-    public string? Title { get; set; } // ? makes Title field nullable.
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    
     [Display(Name = "Release Date")] // Better display name in app
     [DataType(DataType.Date)] // Only display date, not time, in ReleaseDate field below.
+    // DateTimes are inherently required, so don't need [Required]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
+    // decimals, ints, and floats are inherently required, so don't need [Required]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [Required]
+    [StringLength(5)]
+    public string Rating { get; set; } = string.Empty;
 }
