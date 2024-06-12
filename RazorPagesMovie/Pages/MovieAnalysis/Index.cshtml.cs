0d830c4dfb193a5185fe5e5bd18a0ac42c897a7d
mkoelbel <mariackoelbel@gmail.com>
2024-06-12 16:56:25 -0400

    Add 3 graphs displaying movie counts by various fields

--- RazorPagesMovie/Pages/MovieAnalysis/Index.cshtml.cs
+++ RazorPagesMovie/Pages/MovieAnalysis/Index.cshtml.cs
@@ -15,24 +15,39 @@ namespace RazorPagesMovie.Pages.MovieAnalysis
             _context = context;
         }
 
+        public List<string> AllRatings { get; set; }
+        public List<int> MovieCountsByRating { get; set; }
+
         public List<string> AllGenres { get; set; }
         public List<int> MovieCountsByGenre { get; set; }
 
+        public List<string> AllCountries { get; set; }
+        public List<int> MovieCountsByCountry { get; set; }
+
+        public async Task<(List<string>, List<int>)> GetMovieCountsByFieldAsync(string fieldName)
+        {
+            var movieCountsData = await _context.Movie
+                .GroupBy(m => EF.Property<object>(m, fieldName)) // Use EF.Property for accessing dynamic property names
+                .Select(g => new
+                {
+                    FieldValue = g.Key.ToString(), // "Key" is the field we GroupedBy above
+                    Count = g.Count()
+                })
+                .OrderByDescending(g => g.Count)
+                .ToListAsync();
+
+            var fieldValues = movieCountsData.Select(data => data.FieldValue).ToList();
+            var counts = movieCountsData.Select(data => data.Count).ToList();
+
+            return (fieldValues, counts);
+        }
+
         public async Task OnGetAsync()
         {
-            // Query to get the count of movies grouped by genre
-            var genreData = await _context.Movie
-                                          .GroupBy(m => m.Genre)
-                                          .Select(g => new
-                                          {
-                                              Genre = g.Key,
-                                              Count = g.Count()
-                                          })
-                                          .ToListAsync();
-
-            // Process data for the chart
-            AllGenres = genreData.Select(g => g.Genre).ToList();
-            MovieCountsByGenre = genreData.Select(g => g.Count).ToList();
+            // Data to feed graphs
+            (AllRatings, MovieCountsByRating) = await GetMovieCountsByFieldAsync("Rating");
+            (AllGenres, MovieCountsByGenre) = await GetMovieCountsByFieldAsync("Genre");
+            (AllCountries, MovieCountsByCountry) = await GetMovieCountsByFieldAsync("Country");
         }
     }
 }
